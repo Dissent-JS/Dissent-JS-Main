37a980e525c7415ed17469f9d0e8ba44648c0a8d
Rohan Leach <rohanleach4@googlemail.com>
2023-03-15 21:52:22 +0000

    Adding Jest with hideous verbose code in the components. This will have to be refactored

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,4 +2,18 @@ function home() {
     console.log('Home');
 }
 
-module.exports = { home };
+
+try {
+    if (process.env.NODE_ENV === 'test') {
+        console.log('HomeXXXX');
+        module.exports = { home };
+    } else {
+        console.log('We are in production mode');
+    }
+} catch (error) {
+    if (error.name === 'ReferenceError' && error.message.includes('process is not defined')) {
+        console.log('We are in production mode');
+    } else {
+        console.error('An unexpected error occurred:', error);
+    }
+}
