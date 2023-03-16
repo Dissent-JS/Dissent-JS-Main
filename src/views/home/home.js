3c32298f824c2e9816630fa2f290394d0e78c685
Rohan Leach <rohanleach4@googlemail.com>
2023-03-16 07:02:01 +0000

    try statement refactored

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -5,15 +5,11 @@ function home() {
 
 try {
     if (process.env.NODE_ENV === 'test') {
-        console.log('HomeXXXX');
         module.exports = { home };
-    } else {
-        console.log('We are in production mode');
     }
 } catch (error) {
-    if (error.name === 'ReferenceError' && error.message.includes('process is not defined')) {
-        console.log('We are in production mode');
-    } else {
+    if (!(error.name === 'ReferenceError' && error.message.includes('process is not defined'))) {
         console.error('An unexpected error occurred:', error);
     }
 }
+
