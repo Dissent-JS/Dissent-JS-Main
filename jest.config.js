af0dad04099ae976279e6fb2e820e2600a0b36b6
Rohan Leach <rohanleach4@googlemail.com>
2023-03-16 11:28:33 +0000

    The Jest test is ina good state, but the component code is still verbose

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
         '<rootDir>/__tests__/layouts/**/*.test.js',
         '<rootDir>/__tests__/views/**/*.test.js',
     ],
+    transform: {
+        "^.+\\.jsx?$": "babel-jest"
+    },
     moduleDirectories: ['node_modules', '<rootDir>/src'],
     moduleNameMapper: {
         '\\.scss$': 'identity-obj-proxy',
